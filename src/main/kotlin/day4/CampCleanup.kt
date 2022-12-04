6d924d43f626f9c24280a27630e8aa68aad73316
Carst Tankink <ctankink@bol.com>
2022-12-04 06:37:19 +0100

    Day 4 ⭐️⭐️
    Not much to optimize or refactor here

---
+++ src/main/kotlin/day4/CampCleanup.kt
@@ -0,0 +1,33 @@
+package day4
+
+import util.Solution
+
+data class Range(val start: Int, val end: Int) {
+    fun isWithin(other: Range): Boolean =
+        other.start <= this.start && this.end <= other.end
+
+    fun overlaps(other: Range): Boolean = other.start <= this.start && this.start <= other.end
+}
+
+class CampCleanup(fileName: String) : Solution<Pair<Range, Range>, Int>(fileName) {
+    override fun parse(line: String): Pair<Range, Range> {
+        return line.split(',')
+            .map { r ->
+                r.split('-').let {
+                    Range(it[0].toInt(), it[1].toInt())
+                }
+            }.let {
+                Pair(it[0], it[1])
+            }
+    }
+
+    override fun List<Pair<Range, Range>?>.solve1(): Int {
+        return this.filterNotNull()
+            .count { (r1, r2) -> r1.isWithin(r2) || r2.isWithin(r1) }
+    }
+
+    override fun List<Pair<Range, Range>?>.solve2(): Int {
+        return this.filterNotNull()
+            .count { (r1, r2) -> r1.overlaps(r2) || r2.overlaps(r1) }
+    }
+}
