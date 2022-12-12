64f240926652398d42c95b4f53074661f6eefd8d
Carst Tankink <ctankink@bol.com>
2022-12-12 10:43:10 +0100

    Day 12 Optimization because I remembered something about Dijkstra :)

--- src/main/kotlin/day12/HillClimbingAlgorithm.kt
+++ src/main/kotlin/day12/HillClimbingAlgorithm.kt
@@ -14,17 +14,17 @@ class HillClimbingAlgorithm(fileName: String) : Solution<List<Char>, Int>(fileNa
     override fun solve1(data: List<List<Char>>): Int {
         val heightMap = data.toGrid()
         val start = heightMap.entries.first { it.value == 'S' }.key
-        return getShortestPathFromStart(heightMap, start)
+        return getShortestPathFromStart(heightMap, setOf(start))
     }
 
-    private fun getShortestPathFromStart(heightMap: Grid<Char>, start: Point): Int {
+    private fun getShortestPathFromStart(heightMap: Grid<Char>, starts: Set<Point>): Int {
         val end = heightMap.entries.first { it.value == 'E' }.key
 
-        fun getHeight(v: Point) = if (v == start) 'a'.code else if (v == end) 'z'.code else (heightMap[v]?.code ?: 200)
+        fun getHeight(v: Point) = if (heightMap[v] == 'S') 'a'.code else if (v == end) 'z'.code else (heightMap[v]?.code ?: 200)
 
         tailrec fun shortestPath(
             unvisited: Set<Point> = heightMap.keys,
-            distances: Map<Point, Int> = mapOf(start to 0)
+            distances: Map<Point, Int> = starts.associateWith { 0 }
         ): Map<Point, Int> {
             return if (distances.filterKeys { it in unvisited }.isEmpty() || end in distances.keys) distances else {
                 val v = distances.filterKeys { it in unvisited }.minBy { it.value }.key
@@ -53,6 +53,6 @@ class HillClimbingAlgorithm(fileName: String) : Solution<List<Char>, Int>(fileNa
         val heightMap = data.toGrid()
         val starts = heightMap.filterValues { it == 'S' || it == 'a' }.keys
         
-        return starts.minOf { getShortestPathFromStart(heightMap, it) }
+        return getShortestPathFromStart(heightMap, starts)
     }
 }
