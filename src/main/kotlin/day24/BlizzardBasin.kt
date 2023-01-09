d4d663fdad65db51e9129c1063a3930e98176bdb
Carst Tankink <ctankink@bol.com>
2023-01-09 16:24:43 +0100

    Day 24 Parsing

--- src/main/kotlin/day24/BlizzardBasin.kt
+++ src/main/kotlin/day24/BlizzardBasin.kt
@@ -1,8 +1,33 @@
 package day24
 
 import util.Facing
+import util.Solution
 
 sealed interface POI
-data class Blizzard(val facing: Facing)
-class BlizzardBasin {
+data class Blizzard(val facing: Facing) : POI
+object Wall : POI
+object Open : POI
+
+class BlizzardBasin(fileName: String) : Solution<List<POI>, Int>(fileName) {
+    override fun parse(line: String): List<POI> {
+        return line.map {
+            when (it) {
+                '#' -> Wall
+                '.' -> Open
+                '<' -> Blizzard(Facing.LEFT)
+                '>' -> Blizzard(Facing.RIGHT)
+                '^' -> Blizzard(Facing.UP)
+                'v' -> Blizzard(Facing.DOWN)
+                else -> TODO("Unknown character $it")
+            }
+        }
+    }
+
+    override fun solve1(data: List<List<POI>>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun solve2(data: List<List<POI>>): Int {
+        TODO("Not yet implemented")
+    }
 }
